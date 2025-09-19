6bda0be206d02abae877ec7edcf2e01ee5531b26
Andexenn <phanhuynhhung0707@gmail.com>
2025-09-19 17:03:08 +0700

    22. Binding

--- QuanLyDaiLy_MAUI/ViewModels/DaiLyViewModels/TraCuuDaiLyPageViewModel.cs
+++ QuanLyDaiLy_MAUI/ViewModels/DaiLyViewModels/TraCuuDaiLyPageViewModel.cs
@@ -13,14 +13,18 @@ public partial class TraCuuDaiLyPageViewModel : BaseViewModel
 	private readonly IQuanService _quanService;
 	private readonly IDonViTinhService _donViTinhService;
 	private readonly ILoaiDaiLyService _loaiDaiLyService;
+	private readonly IPhieuXuatService _phieuXuatService;
+	private readonly ICTPhieuXuatService _ctPhieuXuatService;
 
-    public TraCuuDaiLyPageViewModel(IDaiLyService daiLyService, IMatHangService matHangService, IQuanService quanService, IDonViTinhService donViTinhService, ILoaiDaiLyService loaiDaiLyService)
+    public TraCuuDaiLyPageViewModel(IDaiLyService daiLyService, IMatHangService matHangService, IQuanService quanService, IDonViTinhService donViTinhService, ILoaiDaiLyService loaiDaiLyService, ICTPhieuXuatService cTPhieuXuatService, IPhieuXuatService phieuXuatService)
 	{
 		_daiLyService = daiLyService;
 		_matHangService = matHangService;
 		_quanService = quanService;
 		_donViTinhService = donViTinhService;
 		_loaiDaiLyService = loaiDaiLyService;
+		_ctPhieuXuatService = cTPhieuXuatService;
+		_phieuXuatService = phieuXuatService;
 
 		_ = LoadDataAsync();
     }
@@ -35,6 +39,8 @@ public partial class TraCuuDaiLyPageViewModel : BaseViewModel
     ObservableCollection<LoaiDaiLy> loaiDaiLies = [];
 	[ObservableProperty]
 	ObservableCollection<DaiLy> daiLies = [];
+	[ObservableProperty]
+	ObservableCollection<DaiLy> filteredDaiLies = [];
 
     [ObservableProperty]
 	private LoaiDaiLy? selectedLoaiDaiLy;
@@ -44,9 +50,53 @@ public partial class TraCuuDaiLyPageViewModel : BaseViewModel
 	private MatHang? selectedMatHang;
 	[ObservableProperty]
 	private DonViTinh? selectedDonViTinh;
+	[ObservableProperty]
+	string maDaiLy = "";
+	[ObservableProperty]
+	string tenLoaiDaiLy = "";
+	[ObservableProperty]
+	string soDienThoai = "";
+	[ObservableProperty]
+	string diaChi = "";
+	[ObservableProperty]
+	string email = "";
 
-
-
+	[ObservableProperty]
+	DateTime ngayTiepNhanFrom = DateTime.Now.AddYears(-1);
+	[ObservableProperty]
+	DateTime ngayTiepNhanTo = DateTime.Now;
+	[ObservableProperty]
+	double noDaiLyFrom = 0;
+	[ObservableProperty]
+	double noDaiLyTo = 1000000000;
+	[ObservableProperty]
+	int maPhieuXuatFrom = 0;
+	[ObservableProperty]
+	int maPhieuXuatTo = 1000000;
+	[ObservableProperty]
+	DateTime ngayLapPhieuXuatFrom = DateTime.Now.AddYears(-1);
+	[ObservableProperty]
+	DateTime ngayLapPhieuXuatTo = DateTime.Now;
+	[ObservableProperty]
+	double tongTienPhieuXuatFrom = 0;
+	[ObservableProperty]
+	double tongTienPhieuXuatTo = 1000000000;
+	[ObservableProperty]
+	int soLuongMatHangXuatFrom = 0;
+	[ObservableProperty]
+	int soLuongMatHangXuatTo = 1000000;
+	[ObservableProperty]
+	double donGiaMatHangXuatFrom = 0;
+	[ObservableProperty]
+	double donGiaMatHangXuatTo = 1000000000;
+	[ObservableProperty]
+	double thanhTienMatHangXuatFrom = 0;
+	[ObservableProperty]
+	double thanhTienMatHangXuatTo = 1000000000;
+	[ObservableProperty]
+	int soLuongTonMatHangXuatFrom = 0;
+	[ObservableProperty]
+	int soLuongTonMatHangXuatTo = 1000000;
 
     [RelayCommand]
 	private async Task GoBack()
@@ -84,6 +134,25 @@ public partial class TraCuuDaiLyPageViewModel : BaseViewModel
 			await Task.Yield();
 			IsLoading = false;
         }
+    }
 
+	[RelayCommand]
+	async Task TraCuuDaiLy()
+	{
+		IsLoading = true;
+		await Task.Yield();
+		try
+		{
+
+		}
+		catch(Exception ex)
+		{
+			await Shell.Current.DisplayAlert("Error", ex.Message, "OK");
+		}
+		finally
+		{
+			await Task.Yield();
+			IsLoading = false;
+        }
     }
 }
