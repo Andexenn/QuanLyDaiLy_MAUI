cae461ca8ffd73518cc29f908be015d7227b05ca
Andexenn <phanhuynhhung0707@gmail.com>
2025-09-22 08:54:17 +0700

    27. Logic 1,2,3

--- QuanLyDaiLy_MAUI/ViewModels/PhieuThuViewModels/LapPhieuThuModalViewModel.cs
+++ QuanLyDaiLy_MAUI/ViewModels/PhieuThuViewModels/LapPhieuThuModalViewModel.cs
@@ -1,14 +1,51 @@
 using CommunityToolkit.Maui.Views;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using QuanLyDaiLy_MAUI.Services;
+using System.Collections.ObjectModel;
+using QuanLyDaiLy_MAUI.Models;
 
 namespace QuanLyDaiLy_MAUI.ViewModels.PhieuThuViewModels;
 
-public class LapPhieuThuModalViewModel : BaseViewModel
+public partial class LapPhieuThuModalViewModel : BaseViewModel
 {
     private Popup? _currentPopup;
-    public LapPhieuThuModalViewModel()
+    private readonly IDaiLyService _daiLyService;
+    //private readonly IPhieuThuService _phieuThuService;
+    public LapPhieuThuModalViewModel(IDaiLyService daiLyService)
 	{
-		
-	}
+		_daiLyService = daiLyService;
+        //_phieuThuService = phieuThuService;
+
+        _ = LoadDataAsync();
+    }
+
+    [ObservableProperty]
+    ObservableCollection<DaiLy> daiLies = [];
+    [ObservableProperty]
+    DaiLy? selectedDaiLy;
+    [ObservableProperty]
+    DateTime ngayThu = DateTime.Now;
+
     public void SetCurrentPopup(Popup popup) => _currentPopup = popup;
 
+    [RelayCommand]
+    private async Task CloseWindow()
+    {
+        if (_currentPopup != null)
+            await _currentPopup.CloseAsync();
+    }
+
+    private async Task LoadDataAsync()
+    {
+        try
+        {
+            var dailies = await _daiLyService.GetAllDaiLyAsync();
+            DaiLies = new ObservableCollection<DaiLy>(dailies);
+        }
+        catch (Exception ex)
+        {
+            await Shell.Current.DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
 }
